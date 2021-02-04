7fe476f36be0f48e105e381857da79a7f1f52144
chaofeng <owen263@gmail.com>
2021-02-04 16:06:04 +0800

    Add comments to test.sh

--- examples/test.sh
+++ examples/test.sh
@@ -7,17 +7,25 @@ docker-compose exec -T pg psql -c "SELECT version()" -U barman postgres
 docker-compose exec -T pgb psql -c "SELECT version()" -U barman postgres
 
 docker-compose exec -T barman gosu barman barman switch-xlog all
-sleep 5
+
+sleep 10
+
+# Barman check
+docker-compose exec -T barman gosu barman barman check all
 docker-compose exec -T barman gosu barman barman check all | grep -vF FAILED &> /dev/null
 
+# Backup
 docker-compose exec -T barman gosu barman barman backup all
 docker-compose exec -T barman gosu barman barman list-backup all
 docker-compose exec -T barman gosu barman tail -n 100 /var/log/barman/barman.log
 
-# docker-compose exec -T barman echo \$PG_SERVERS
+# Check config
+
 docker-compose exec -T barman ls /etc/barman.d/
 docker-compose exec -T barman cat /etc/barman.d/pg.conf
 docker-compose exec -T barman cat /etc/barman.d/pgb.conf
+
+# Recover
 
 docker-compose exec -T barman gosu barman barman recover pg-ssh first /var/lib/postgresql/data/recovered.ssh --remote-ssh-command "ssh postgres@pg"
 docker-compose exec -T pg ls /var/lib/postgresql/data/recovered.ssh
