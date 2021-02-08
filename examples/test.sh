9e994875d45e36e9bfdc530a98a9bd18c3ea367d
chaofeng <owen263@gmail.com>
2021-02-08 14:48:17 +0800

    Optimize test.sh, test back and recovery both on ssh and streaming

--- examples/test.sh
+++ examples/test.sh
@@ -2,6 +2,8 @@
 
 set -v
 
+echo ==========Check general
+
 docker-compose ps
 docker-compose exec -T pg psql -c "SELECT version()" -U barman postgres
 docker-compose exec -T pgb psql -c "SELECT version()" -U barman postgres
@@ -17,32 +19,36 @@ docker-compose exec -T barman gosu barman barman switch-xlog --force --archive -
 docker-compose exec -T barman gosu barman barman check all
 docker-compose exec -T barman gosu barman barman check all | grep -vF FAILED &> /dev/null
 
-# Check config
+echo ==========Check config
 
 docker-compose exec -T barman ls /etc/barman.d/
 docker-compose exec -T barman cat /etc/barman.d/pg.conf
 docker-compose exec -T barman cat /etc/barman.d/pgb.conf
 
+echo ==========Check backup and recover
+
 for i in pg pgb
 do
 
+for j in ssh streaming
+do
+
+echo =====$i-$j
+
 # Backup
-docker-compose exec -T barman gosu barman barman backup $i-streaming
-docker-compose exec -T barman gosu barman barman list-backup $i-streaming
+docker-compose exec -T barman gosu barman barman backup -w $i-$j
+docker-compose exec -T barman gosu barman barman list-backup $i-$j
+echo
 docker-compose exec -T barman gosu barman tail -n 50 /var/log/barman/barman.log
+echo
 
 # Recover
 
-# docker-compose exec -T barman gosu barman barman recover pg-ssh first /var/lib/postgresql/data/recovered.ssh --remote-ssh-command "ssh postgres@pg"
-# docker-compose exec -T pg ls /var/lib/postgresql/data/recovered.ssh
+target=$(docker-compose exec -T barman gosu barman barman list-backup $i-$j | head -n 1 | cut -d ' ' -f2)
+echo Recover target: $target
 
-docker-compose exec -T barman gosu barman barman recover $i-streaming first /var/lib/postgresql/data/recovered.streaming --remote-ssh-command "ssh postgres@$i"
-docker-compose exec -T $i ls /var/lib/postgresql/data/recovered.streaming
+docker-compose exec -T barman gosu barman barman recover $i-$j $target /var/lib/postgresql/data/recovered.$j --remote-ssh-command "ssh postgres@$i"
+docker-compose exec -T $i ls /var/lib/postgresql/data/recovered.$j
 
-# docker-compose exec -T barman gosu barman barman recover pgb-ssh first /var/lib/postgresql/data/recovered.ssh --remote-ssh-command "ssh postgres@pgb"
-# docker-compose exec -T pgb ls /var/lib/postgresql/data/recovered.ssh
-# 
-# docker-compose exec -T barman gosu barman barman recover pgb-streaming first /var/lib/postgresql/data/recovered.streaming --remote-ssh-command "ssh postgres@pgb"
-# docker-compose exec -T pgb ls /var/lib/postgresql/data/recovered.streaming
-
+done
 done
